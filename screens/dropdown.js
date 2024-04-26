9cf1256bb465a132307b57c089f416c6e7f4453e
[Yahiyamuhammed] <[yahiyamuhammed98@gmail.com]>
2024-04-26 11:38:12 +0530

    get the best dropdown list

--- screens/dropdown.js
+++ screens/dropdown.js
@@ -1,115 +1,36 @@
-// import * as React from 'react';
 import React, { useState } from 'react';
-import { View, FlatList, Text, TouchableOpacity ,TouchableWithoutFeedback,Keyboard} from 'react-native';
-import style from './styles'
+import { View, Text, TouchableWithoutFeedback, FlatList, Keyboard } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import { SelectList } from 'react-native-dropdown-select-list'
 
-import { TextInput ,IconButton } from 'react-native-paper';
-
-   
-    const MyComponent = ({ handleChange }) => {
-        
-    const [text, setText] = React.useState("");
-    const [isFocused, setIsFocused] = useState(false);
-    const [isClearable, setIsClearable] = useState(false);
-
-
+const MyComponent = () => {
   
 
-  // Sample list data for demonstration
-  const dataList = [
-    { id: 1, label: 'Option 1' },
-    { id: 2, label: 'Option 2' },
-    { id: 3, label: 'Option 3' },
-    { id: 4, label: 'Option 4' },
-    { id: 5, label: 'Option 5' },
-  ];
 
-  const handleFocus = () => {
-    setIsFocused(true);
+
+  const [selected, setSelected] = React.useState("");
+  
+  const data = [
+      {key:'1', value:'Mobiles', disabled:true},
+      {key:'2', value:'Appliances'},
+      {key:'3', value:'Cameras'},
+      {key:'4', value:'Computers', disabled:true},
+      {key:'5', value:'Vegetables'},
+      {key:'6', value:'Diary Products'},
+      {key:'7', value:'Drinks'},
+  ]
+  const clearSelection = () => {
+    setSelected(""); // Clear the selected value
   };
-//   React.useEffect(() => {
-//     console.log(selectedItem);
-//   }, [selectedItem]);
-  
 
-
-  const handleBlur = () => {
-    setTimeout(() => {
-        setIsFocused(false); // Set isFocused to false after a delay
-      }, 2000);  };
-
-  const handleSelectItem = (item) => {
-    console.log(item.label);
-    // setSelectedItem(item); // Add this line
-        // Call the passed-in handleChange function with the selected item
-    handleChange(item);
-    setText(item.label);
-    setIsClearable(true);
-    setIsFocused(false);
-    // setTimeout(() => {
-    //     setIsFocused(false); // Set isFocused to false after a delay
-    //   }, 200);  
-    };
-
-    const handleClear = () => {
-        setText("");
-        setIsClearable(false)
-    };
-
-
-  const renderDropdownList = () => (
-    <FlatList 
-
-
-      data={dataList}
-      style={style.select}
-      renderItem={({ item }) => (
-        <TouchableOpacity onPress={() => handleSelectItem(item)} 
-        >
-          <Text style={style.selectItems}>{item.label}</Text>
-        </TouchableOpacity>
-      )}
-      keyExtractor={(item) => item.id.toString()}
+  return(
+    <SelectList 
+        setSelected={(val) => setSelected(val)} 
+        data={data} 
+        save="value"
     />
-  );
+  )
 
-
-  return (
-    <TouchableWithoutFeedback onPress={handleFocus}
-    onBlur={handleBlur}
-    style={style.test}>
-    <View >
-        <View style={style.testborder}>
-      <TextInput
-    //   style={style.selectInput}
-
-      style={[style.selectInput, !isClearable && style.fullWidthInput]}
-
-        mode="outlined"
-        label="Email"
-        value={text}
-        isClearable={isClearable}
-        onChangeText={(text) => setText(text)}
-        onFocus={handleFocus}
-       />
-      
-      {isClearable && ( // Show clear button only if isClearable is true
-            // console.log('isClearable:', isClearable), // Add this line
-
-                <IconButton // Use IconButton from react-native-paper
-                    icon="close"
-                    onPress={handleClear}
-                    style={style.clrbutton}
-                />
-            )}
-            </View>
-
-            {isFocused && renderDropdownList()}
-      
-    </View>
-  </TouchableWithoutFeedback>
-    
-  );
 };
 
 export default MyComponent;
